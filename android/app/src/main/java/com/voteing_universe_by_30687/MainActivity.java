74cc8d3b0e3113e030e8638c190f6ecfc8f817eb
Crowdbotics <team@crowdbotics.com>
2021-09-19 22:40:42 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/voteing_universe_by_30687/MainActivity.java
@@ -0,0 +1,15 @@
+package com.voteing_universe_by_30687;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "voteing_universe_by_30687";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "voteing_universe_by_30687Identifier",
+  url: "https://voteing_universe_by_30687Identifier.botics.co",
+  api: "https://voteing_universe_by_30687Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
